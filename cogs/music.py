130a3ca2868d42e8b6fd32d6cb432733c0ec9997
Maj Soklič <majs.1236@gmail.com>
2023-03-27 21:14:18 +0200

    [Music/create_embed] used fstrings over format

--- cogs/music.py
+++ cogs/music.py
@@ -146,17 +146,16 @@ class Song:
             discord.
             Embed(
                 title='Now playing',
-                description='```css\n{0.source.title}\n```'.format(self),
-                color=discord.Color.blurple()).
-            add_field(name='Duration', value=self.source.duration).
-            add_field(name='Requested by', value=self.requester.mention).
-            add_field(
+                description=f'```css\n{self.source.title}\n```',
+                color=discord.Color.blurple())
+            .add_field(name='Duration', value=self.source.duration)
+            .add_field(name='Requested by', value=self.requester.mention)
+            .add_field(
                 name='Uploader',
-                value='[{0.source.uploader}]({0.source.uploader_url})'.format(
-                    self)).add_field(
-                name='URL', value='[Click]({0.source.url})'.format(self)).
-            set_thumbnail(url=self.source.thumbnail).set_author(
-                name=self.requester.name, icon_url=self.requester.avatar.url))
+                value=f"[{self.source.uploader}]({self.source.uploader_url})")
+            .add_field(name='URL', value=f'[Click]({self.source.url})')
+            .set_thumbnail(url=self.source.thumbnail)
+            .set_author(name=self.requester.name, icon_url=self.requester.avatar.url))
         return embed
 
 
