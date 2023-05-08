4b28cccb68d3dc81edf597ecf2ee34c9bd107ab4
Maj Soklič <majs.1236@gmail.com>
2023-05-08 12:07:45 +0200

    [music/_toggle] fix: addendum to db385f5

--- cogs/music.py
+++ cogs/music.py
@@ -404,7 +404,7 @@ class Music(commands.Cog):
         if ctx.voice_state.is_playing and ctx.voice_state.voice.is_paused():
             ctx.voice_state.voice.resume()
             await ctx.message.add_reaction("⏯")
-        elif ctx.voice_state.is_playing():
+        elif ctx.voice_state.is_playing:
             ctx.voice_state.voice.pause()
             await ctx.message.add_reaction("⏯")
         else:
