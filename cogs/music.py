217b0c882fc4a37f9a3a3b1c6a4b05f488a575f0
Maj Soklič <majs.1236@gmail.com>
2023-08-29 19:13:27 +0200

    [music:_loop] refactor: simplify if statement

--- cogs/music.py
+++ cogs/music.py
@@ -544,10 +544,7 @@ class Music(commands.Cog):
 
         # Inverse boolean value to loop and unloop.
         ctx.voice_state.loop = not ctx.voice_state.loop
-        if ctx.voice_state.loop:
-            await ctx.message.add_reaction("✅")
-        else:
-            await ctx.message.add_reaction("❎")  # when looping disabled
+        await ctx.message.add_reaction("✅" if ctx.voice_state.loop else "❎")# when looping disabled
 
     @commands.command(name="play", aliases=["p"])
     async def _play(self, ctx: commands.Context, *, search: str):
